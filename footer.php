d6da9b3499da1f98b0b658121af73ebe82a83ea2
Adarsh Anand <125790093+AdarshAnand10@users.noreply.github.com>
2025-02-15 02:20:10 +0530

    footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-zinc-900 text-white py-12">
+    <div class="container mx-auto px-4 text-center">
+        <p>&copy; <?php echo date('Y'); ?> Little Miami Brewing Company. All rights reserved.</p>
+    </div>
+    <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
